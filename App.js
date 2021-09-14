ce81fa6540b22e2e6a2768c8065ca7cb35e4ea70
Rodolphe <dutchr@live.fr>
2021-09-14 14:30:45 +0200

    test API TMDB

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const getPopularMovies = async() => {
+  const result = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=4e2a93812004fddd622fb5e91194b8a9')
+  console.log(JSON.stringify(result.data.results[0], null, 2));
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  getPopularMovies()
+  return (
+    <View></View>
+  )
+}
+
+
+export default App;
