b4c865e0232615bfef324b633d53cd3870a51f32
Rodolphe <dutchr@live.fr>
2021-09-14 14:58:42 +0200

    Adding error handler

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import axios from "axios";
 
@@ -12,14 +12,21 @@ const getPopularMovies = async () => {
 
 const App = () => {
   const [movie, setMovie] = useState('');
-  getPopularMovies().then((movies) => {
-    setMovie(movies[0]);
-  });
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    getPopularMovies().then((movies) => {
+      setMovie(movies[0]);
+    }).catch((error) => {
+      setError(error);
+    });
+  }, [])
   return (
     <View>
       <Text> Nom du Film : {movie.original_title} </Text>
       <Text> Langue: {movie.original_language} </Text>
       <Text> Date de sortie:  {movie.release_date} </Text>
+       {error && <Text style={{color:'red'}}> Erreur sur le serveur </Text>}
     </View>
   );
 };
